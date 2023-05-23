a263c0a4fd4e29b1f68f57820d564f34baba0309
AndrewB4y <andresbaymon@gmail.com>
2023-05-23 16:31:57 -0500

    [fix] removing auto_suggest feature from wikipedia function

--- mylib/logic.py
+++ mylib/logic.py
@@ -4,5 +4,5 @@ import wikipedia
 def wiki(name="War Goddess", lenght=1):
     """This is a wikipedia fetcher"""
 
-    my_wiki = wikipedia.summary(name, lenght)
+    my_wiki = wikipedia.summary(name, lenght, auto_suggest = False)
     return my_wiki
